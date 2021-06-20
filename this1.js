cb051dda4739abb291cbfb1d00909a63d3714b0d
Pranesh <praneshmeher@gmail.com>
2021-06-20 16:39:48 +0530

    commit

---
+++ this1.js
@@ -0,0 +1,14 @@
+const person = {
+    firstname:'Pranesh',
+    lastname:'Meher',
+    fullname:function(){
+        console.log(this.firstname+' '+this.lastname)
+    }
+}
+
+person.fullname()
+
+const getfullname = person.fullname
+
+getfullname()
+
