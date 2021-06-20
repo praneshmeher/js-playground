f779f78945849f9c73573e724ae794c639c3ee50
Pranesh <praneshmeher@gmail.com>
2021-06-20 17:23:30 +0530

    commit

--- bind.js
+++ bind.js
@@ -1,6 +1,3 @@
-// The bind() method creates a new function that, 
-// when called, has its this keyword set to the provided value,
-// with a given sequence of arguments preceding any provided when the new function is called.
 
 const john = {
     name:'john'
@@ -10,12 +7,12 @@ const jane = {
     name:'jane'
 }
 
-function greeting(){
-    console.log('hello', this.name)
+function greeting(arg){
+    console.log('hello', this.name, arg)
 }
 
-let greetjohn = greeting.bind(john)
+let greetjohn = greeting.bind(john, 'rambo')
 console.log(greetjohn())
 
-let greetjane = greeting.bind(jane)
+let greetjane = greeting.bind(jane, 'rambo')
 console.log(greetjane())
