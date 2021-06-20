56fcb6f7c7edde3df51a1d22632db40afaaebacf
Pranesh <praneshmeher@gmail.com>
2021-06-20 11:06:43 +0530

    commit

---
+++ function.js
@@ -0,0 +1,21 @@
+
+// function statement || function declaration
+
+function satement() {
+    console.log('satement')
+}
+
+// function expression
+
+var expression = function () {
+    console.log('expression')
+}
+
+// named function expression
+
+var named = function abc(){
+    console.log('named')
+}
+
+abc()
+
